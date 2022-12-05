30c7eafadd2d8d386aeb9b26d33d61d277d8d3bf
rainbrry <ade.usman403@gmail.com>
2022-12-05 11:48:51 +0700

    change purchasing schema

--- backend/src/app/schemas/purchasings.schema.js
+++ backend/src/app/schemas/purchasings.schema.js
@@ -4,19 +4,21 @@ const { Schema, model } = mongoose;
 
 const PurchasingSchema = new Schema(
 	{
-		user: { type: Schema.Types.ObjectId, ref: "User" },
-		items: [
+		user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		products: [
 			{
+				id: false,
 				product: { type: Schema.Types.ObjectId, ref: "Product" },
 				price: { type: Number, required: true },
 				qty: { type: Number, required: true },
 			},
 		],
 		grandTotal: { type: Number, required: true },
-		status: { type: String, required: true },
+		status: { type: String, required: true, default: "success" },
 		date: { type: Date, default: Date.now },
 	},
 	{
+		timestamps: true,
 		versionKey: false,
 	}
 );
