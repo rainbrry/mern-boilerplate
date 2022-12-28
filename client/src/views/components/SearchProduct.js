43b2867432caa2d29f14cfc97e68ae4e0fc5e651
rainbrry <ade.usman403@gmail.com>
2022-12-28 10:46:03 +0700

    fix search product

--- client/src/views/components/SearchProduct.js
+++ client/src/views/components/SearchProduct.js
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
-import axios from "../../helpers/axios";
 import { rupiah } from "../../helpers/currency";
+import { useLazySearchProductQuery } from "../../services/api/products";
 import Modal from "./Modal";
 
-export default function SearchProduct({ addItem, cartType }) {
-	const [products, setProducts] = useState([]);
+export default function SearchProduct({ selectProduct, cartType }) {
 	const [openModal, setOpenModal] = useState(false);
+	const [search, { data: products }] = useLazySearchProductQuery();
 
 	const addToCart = (product) => {
-		addItem(product);
+		selectProduct(product);
 		setOpenModal(false);
 	};
 
 	const queryProduct = async (query) => {
-		await axios.get(`search-product?query=${query}`).then((res) => {
-			setProducts(res.data.data);
-		});
+		await search(query);
 		setOpenModal(true);
 	};
 
@@ -48,7 +46,7 @@ export default function SearchProduct({ addItem, cartType }) {
 							<div className="px-4 py-2 w-1/12">Action</div>
 						</div>
 
-						{!products.length && (
+						{products && !products.length && (
 							<div className="flex flex-row py-5 w-full justify-center">
 								<div className="px-3 py-2 w-full font-medium text-center">
 									Data tidak ditemukan
@@ -56,54 +54,55 @@ export default function SearchProduct({ addItem, cartType }) {
 							</div>
 						)}
 
-						{Object.values(products).map((product, index) => (
-							<div
-								key={index}
-								tabIndex={0}
-								className="px-4 py-2 flex w-full items-center focus:bg-blue-500 focus:opacity-80 focus:text-white outline-none overflow-hidden rounded"
-								onKeyUp={(e) => e.key === "Enter" && addToCart(product)}
-								autoFocus={true}
-							>
-								<div className="flex flex-row w-full items-center justify-center">
-									<div className="py-1 w-6/12 truncate capitalize">
-										{product.name}
-									</div>
-
-									{cartType === "purchasing" && (
-										<div className="px-4 py-1 w-2/12 truncate">
-											{rupiah(product.purchasePrice)}
+						{products &&
+							products.map((product, index) => (
+								<div
+									key={index}
+									tabIndex={0}
+									className="px-4 py-2 flex w-full items-center focus:bg-blue-500 focus:opacity-80 focus:text-white outline-none overflow-hidden rounded"
+									onKeyUp={(e) => e.key === "Enter" && addToCart(product)}
+									autoFocus={true}
+								>
+									<div className="flex flex-row w-full items-center justify-center">
+										<div className="py-1 w-6/12 truncate capitalize">
+											{product.name}
 										</div>
-									)}
 
-									<div className="px-4 py-1 w-2/12 truncate">
-										{rupiah(product.salesPrice)}
-									</div>
-									<div className="px-4 py-1 w-1/12 flex justify-center truncate">
-										{product.stock}
-									</div>
-									<div className="px-4 w-1/12">
-										<button
-											onClick={() =>
-												addItem({
-													product: product._id,
-													name: product.name,
-													price: product.purchasePrice,
-												})
-											}
-											tabIndex={-1}
-											className={`px-4 py-1 text-white rounded shadow-lg ${
-												product.stock < 1 && cartType === "selling"
-													? "bg-gray-500"
-													: "bg-green-500"
-											}`}
-											disabled={product.stock < 1 && cartType === "selling"}
-										>
-											Pilih
-										</button>
+										{cartType === "purchasing" && (
+											<div className="px-4 py-1 w-2/12 truncate">
+												{rupiah(product.purchasePrice)}
+											</div>
+										)}
+
+										<div className="px-4 py-1 w-2/12 truncate">
+											{rupiah(product.salesPrice)}
+										</div>
+										<div className="px-4 py-1 w-1/12 flex justify-center truncate">
+											{product.stock}
+										</div>
+										<div className="px-4 w-1/12">
+											<button
+												onClick={() =>
+													selectProduct({
+														product: product._id,
+														name: product.name,
+														price: product.purchasePrice,
+													})
+												}
+												tabIndex={-1}
+												className={`px-4 py-1 text-white rounded shadow-lg ${
+													product.stock < 1 && cartType === "selling"
+														? "bg-gray-500"
+														: "bg-green-500"
+												}`}
+												disabled={product.stock < 1 && cartType === "selling"}
+											>
+												Pilih
+											</button>
+										</div>
 									</div>
 								</div>
-							</div>
-						))}
+							))}
 					</div>
 				</Modal>
 			</div>
