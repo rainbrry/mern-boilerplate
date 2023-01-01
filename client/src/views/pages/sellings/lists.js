3933458b40f57fb57a9307854c6da74e461f0b82
rainbrry <ade.usman403@gmail.com>
2023-01-01 17:32:37 +0700

    add loading state

--- client/src/views/pages/sellings/lists.js
+++ client/src/views/pages/sellings/lists.js
@@ -8,7 +8,7 @@ import {
 import SellingDetail from "./detail";
 
 export default function ListSellings() {
-	const { data: sellings = [] } = useGetSellingsQuery();
+	const { data: sellings = [], isLoading, isError } = useGetSellingsQuery();
 	const [deleteSelling] = useDeleteSellingMutation();
 
 	return (
@@ -35,7 +35,15 @@ export default function ListSellings() {
 						</tr>
 					</thead>
 					<tbody className="text-md divide-y text-left divide-gray-100">
-						{!sellings.length && (
+						{isLoading ? (
+							<tr>
+								<td className="p-4 whitespace-nowrap" colSpan="5">
+									<div className="text-center text-lg font-semibold mt-56">
+										Loading...
+									</div>
+								</td>
+							</tr>
+						) : isError || !sellings.length ? (
 							<tr>
 								<td className="p-4 whitespace-nowrap" colSpan="5">
 									<div className="text-center text-lg font-semibold mt-56">
@@ -43,72 +51,72 @@ export default function ListSellings() {
 									</div>
 								</td>
 							</tr>
+						) : (
+							sellings.map((selling, index) => {
+								return (
+									<tr
+										key={index}
+										className="border-b-2 border-gray-200 hover:bg-gray-300"
+									>
+										<td className="px-4 py-2 whitespace-nowrap">
+											<div className="font-medium text-gray-800 capitalize">
+												{new Date(selling.createdAt).toLocaleString("id-ID", {
+													year: "numeric",
+													month: "numeric",
+													day: "numeric",
+													hour: "numeric",
+													minute: "numeric",
+												})}
+											</div>
+										</td>
+
+										<td className="px-4 py-2 whitespace-nowrap">
+											<div className="font-normal">{selling._id}</div>
+										</td>
+
+										<td className="px-4 py-2 whitespace-nowrap">
+											<div className="font-normal">
+												{rupiah(selling.grandTotal)}
+											</div>
+										</td>
+
+										<td className="px-4 py-2 whitespace-nowrap text-center w-28">
+											<div
+												className={`px-4 font-normal rounded-full text-white flex items-center justify-center ${
+													selling.status === "sold"
+														? "bg-green-500"
+														: "bg-red-500"
+												}`}
+											>
+												{selling.status === "sold" ? "Sold" : "Returned"}
+											</div>
+										</td>
+
+										<td className="p-2 whitespace-nowrap">
+											<div className="text-md flex gap-2 justify-center items-center">
+												<SellingDetail selling={selling} />
+
+												{selling.status === "sold" ? (
+													<NavLink
+														to={`/return-selling/${selling._id}`}
+														className="px-4 py-0.5 bg-yellow-500 rounded-lg shadow text-white hover:bg-yellow-700"
+													>
+														Retur
+													</NavLink>
+												) : (
+													<button
+														onClick={() => deleteSelling(selling._id)}
+														className="px-4 py-0.5 bg-red-600 rounded-lg shadow text-white hover:bg-red-700"
+													>
+														Hapus
+													</button>
+												)}
+											</div>
+										</td>
+									</tr>
+								);
+							})
 						)}
-
-						{sellings.map((selling, index) => {
-							return (
-								<tr
-									key={index}
-									className="border-b-2 border-gray-200 hover:bg-gray-300"
-								>
-									<td className="px-4 py-2 whitespace-nowrap">
-										<div className="font-medium text-gray-800 capitalize">
-											{new Date(selling.createdAt).toLocaleString("id-ID", {
-												year: "numeric",
-												month: "numeric",
-												day: "numeric",
-												hour: "numeric",
-												minute: "numeric",
-											})}
-										</div>
-									</td>
-
-									<td className="px-4 py-2 whitespace-nowrap">
-										<div className="font-normal">{selling._id}</div>
-									</td>
-
-									<td className="px-4 py-2 whitespace-nowrap">
-										<div className="font-normal">
-											{rupiah(selling.grandTotal)}
-										</div>
-									</td>
-
-									<td className="px-4 py-2 whitespace-nowrap text-center w-28">
-										<div
-											className={`px-4 font-normal rounded-full text-white flex items-center justify-center ${
-												selling.status === "sold"
-													? "bg-green-500"
-													: "bg-red-500"
-											}`}
-										>
-											{selling.status === "sold" ? "Sold" : "Returned"}
-										</div>
-									</td>
-
-									<td className="p-2 whitespace-nowrap">
-										<div className="text-md flex gap-2 justify-center items-center">
-											<SellingDetail selling={selling} />
-
-											{selling.status === "sold" ? (
-												<NavLink
-													to={`/return-selling/${selling._id}`}
-													className="px-4 py-0.5 bg-yellow-500 rounded-lg shadow text-white hover:bg-yellow-700"
-												>
-													Retur
-												</NavLink>
-											) : (
-												<button
-													onClick={() => deleteSelling(selling._id)}
-													className="px-4 py-0.5 bg-red-600 rounded-lg shadow text-white hover:bg-red-700"
-												>
-													Hapus
-												</button>
-											)}
-										</div>
-									</td>
-								</tr>
-							);
-						})}
 					</tbody>
 				</table>
 			</div>
